7ebc291c80dc2b26cd259b283bca325cf43c0382
Ayub Malik <ayub.malik@gmail.com>
2024-09-11 20:42:52 +0100

    Adding neotest no ui

--- init.lua
+++ init.lua
@@ -736,6 +736,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        go = { 'gofmt' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -1,6 +1,25 @@
 return {
   'nvim-neotest/neotest',
   dependencies = {
-    'nvim-neotest/neotest-plenary',
+    'nvim-lua/plenary.nvim',
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-neotest/nvim-nio',
+    'nvim-neotest/neotest-go',
   },
+  opts = {},
+
+  config = function()
+    require('neotest').setup {
+      adapters = {
+        require 'neotest-go',
+      },
+      output_panel = {
+        enabled = true,
+        open = 'botright split | resize 15',
+      },
+      quickfix = {
+        open = false,
+      },
+    }
+  end,
 }
